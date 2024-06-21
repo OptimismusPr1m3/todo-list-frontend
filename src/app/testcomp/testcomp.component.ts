49ae30d8a50e29c45616d53e25bb31749ef98a00
Sebastian Wolff <bastiwolff432@gmail.com>
2024-06-21 09:54:49 +0200

    testcomp for ftp server

---
+++ src/app/testcomp/testcomp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testcomp',
+  standalone: true,
+  imports: [],
+  templateUrl: './testcomp.component.html',
+  styleUrl: './testcomp.component.scss'
+})
+export class TestcompComponent {
+
+}
